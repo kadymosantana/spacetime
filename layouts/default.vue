9e69e3eceb4febe54dcb0ae4630f23d86e990b40
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-05-23 21:08:14 +0000

    fix: Fixing bg image path

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@ const isAuthenticated = computed(() => {
   <div class="grid w-screen grid-cols-1 bg-gray-900 text-gray-100 md:h-screen md:grid-cols-2">
     <!-- Left -->
     <div
-      class="relative flex flex-col justify-between gap-20 overflow-hidden border-r border-white/10 bg-[url('./bg-stars.svg')] bg-cover p-8 md:h-screen lg:px-28 lg:py-16"
+      class="relative flex flex-col justify-between gap-20 overflow-hidden border-r border-white/10 bg-[url('/bg-stars.svg')] bg-cover p-8 md:h-screen lg:px-28 lg:py-16"
     >
       <!-- Blur effect -->
       <div
@@ -28,7 +28,7 @@ const isAuthenticated = computed(() => {
     <!-- Right -->
     <div
       :class="{ 'flex h-screen items-center justify-center': !isAuthenticated }"
-      class="relative overflow-y-auto bg-[url('./bg-stars.svg')] p-8 md:p-16"
+      class="relative overflow-y-auto bg-[url('/bg-stars.svg')] p-8 md:p-16"
     >
       <!-- Stripes effect -->
       <div class="absolute bottom-0 left-0 top-0 w-2 bg-stripes md:hidden"></div>
